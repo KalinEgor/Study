ebb50c69f1c21e3bfb95d3c75b5a86c5fff76260
KalinEgor <94646164+KalinEgor@users.noreply.github.com>
2021-11-22 16:03:50 +0300

    Delete TestAuthor.java

--- TestAuthor.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class TestAuthor {
-
-    public static void main(String[] args) {
-	Author obj1 = new Author("Egor Kalinichenko","egor.kalinichenko.2003@mail.ru",'M');
-    System.out.println(obj1);
-    }
-}
